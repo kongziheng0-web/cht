d2e73f15c33abc69b6e645770c68791b6ec3d697
kongziheng0-web <kongziheng0@gmail.com>
2026-05-15 15:28:53 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,27 @@
-const foods = [
-  {name:"番茄炒蛋",type:"热菜",time:10},
-  {name:"红烧肉",type:"热菜",time:60},
-  {name:"酸辣土豆丝",type:"凉菜",time:15},
-  {name:"米饭",type:"主食",time:20},
-  {name:"面条",type:"主食",time:10}
-];
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  databaseURL: "YOUR_DB_URL"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+const ref = db.ref("familyKitchen");
+
+let state = {
+  menu: [],
+  foods: [],
+  systemName: "我的厨房",
+  banner: "",
+  avatar: ""
+};
+
+ref.on("value",snap=>{
+  if(snap.val()){
+    state = snap.val();
+    render();
+  }
+});
+
+function save(){
+  ref.set(state);
+}
